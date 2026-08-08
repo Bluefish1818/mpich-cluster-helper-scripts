0c605f2ddc204dead8e4581227fa70449d5ad8aa
Aaron Liu <105533113+Bluefish1818@users.noreply.github.com>
2026-08-07 22:52:08 -0700

    Create 5trials_osu_latency.sh
    Run OSU Latency benchmark for 2 nodes, with 1 process on each node.

---
+++ 5trials_osu_latency.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+cmd=(
+    mpiexec
+    -n 2
+    -f machinefile_cross_2n_x_1p
+    -iface eno1
+    /mirror/mpiu/osu_latency_shortcut
+)
+
+tmpdir=$(mktemp -d)
+trap 'rm -rf "$tmpdir"' EXIT
+
+"${cmd[@]}" 2>&1 | tee "$tmpdir/raw_sizes"
+awk '/^[0-9]/ {print $1}' "$tmpdir/raw_sizes" > "$tmpdir/sizes"
+
+files=("$tmpdir/sizes")
+
+for i in {1..5}; do
+    "${cmd[@]}" 2>&1 | tee "$tmpdir/raw_run$i"
+    awk '/^[0-9]/ {print $2}' "$tmpdir/raw_run$i" > "$tmpdir/run$i"
+
+    files+=("$tmpdir/run$i")
+done
+
+output="osu_latency_results.tsv"
+paste "${files[@]}" | tee "$output"
+echo "Results saved to: $(pwd)/$output"
